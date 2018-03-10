1aac926aa13806fb5a13182306403cd841e964c0
Zhou,Lingjun <zjjhyzzjc@163.com>
2018-03-10 08:31:54 +0800

    添加注释
    Collections.sort,.reverse
    静态全局变量，static HashMap,ArrayList

--- basic level/1005. 继续(3n+1)猜想 (25)-0.29.java
+++ basic level/1005. 继续(3n+1)猜想 (25)-0.29.java
@@ -1,4 +1,4 @@
-/*
+
 卡拉兹(Callatz)猜想已经在1001中给出了描述。在这个题目里，情况稍微有些复杂。
 当我们验证卡拉兹猜想的时候，为了避免重复计算，可以记录下递推过程中遇到的每一个数。例如对n=3进行验证的时候，我们需要计算3、5、8、4、2、1，则当我们对n=5、8、4、2进行验证的时候，就可以直接判定卡拉兹猜想的真伪，而不需要重复计算，因为这4个数已经在验证3的时候遇到过了，我们称5、8、4、2是被3“覆盖”的数。我们称一个数列中的某个数n为“关键数”，如果n不能被数列中的其他数字所覆盖。
 现在给定一系列待验证的数字，我们只需要验证其中的几个关键数，就可以不必再重复验证余下的数字。你的任务就是找出这些关键数字，并按从大到小的顺序输出它们。
@@ -9,8 +9,9 @@
 3 5 6 7 8 11
 输出样例：
 7 6
-*/
 
+//Collections.sort,.reverse
+//静态全局变量，static HashMap,ArrayList
 import java.util.ArrayList;
 
 import java.util.Collections;
@@ -45,7 +46,7 @@ public class pat_1005 {
 		}
 
 		System.out.println(output);
-		
+
 	}
 
 	public static int Callatz(int input_number) {

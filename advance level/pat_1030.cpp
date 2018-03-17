df7082a949f60ab64b194b6fc900f7e6159af815
Zhou,Lingjun <zjjhyzzjc@163.com>
2018-03-17 23:36:25 +0800

    （30-0.45）Dijkstra算法 + DFS，最短路径，边权

--- advance level/pat_1030.cpp
+++ advance level/pat_1030.cpp
@@ -1,3 +1,24 @@
+
+1030 Travel Plan (30)
+A traveler s map gives the distances between cities along the highways, together with the cost of each highway. Now you are supposed to write a program to help a traveler to decide the shortest path between his/her starting city and the destination. If such a shortest path is not unique, you are supposed to output the one with the minimum cost, which is guaranteed to be unique.
+Input Specification:
+Each input file contains one test case. Each case starts with a line containing 4 positive integers N, M, S, and D, where N (<=500) is the number of cities (and hence the cities are numbered from 0 to N-1); M is the number of highways; S and D are the starting and the destination cities, respectively. Then M lines follow, each provides the information of a highway, in the format:
+City1 City2 Distance Cost
+where the numbers are all integers no more than 500, and are separated by a space.
+Output Specification:
+For each test case, print in one line the cities along the shortest path from the starting point to the destination, followed by the total distance and the total cost of the path. The numbers must be separated by a space and there must be no extra space at the end of output.
+Sample Input
+4 5 0 3
+0 1 1 20
+1 3 2 30
+0 3 4 10
+0 2 2 20
+2 3 1 20
+Sample Output
+0 2 3 3 40
+
+
+
 #include<iostream>
 #include<vector>
 #include<cstring>
@@ -47,7 +68,7 @@ void dfs(int start, int end) {
 int main() {
 	int vertex_num, edge_num, start, end;
 	cin >> vertex_num >> edge_num >> start >> end;
-	//初始化图
+	//鍒濆鍖栧浘
 	for (int i = 0; i < vertex_num; i++) {
 		for (int j = 0; j < vertex_num; j++) {
 			if (i == j) {
@@ -61,7 +82,7 @@ int main() {
 		}
 	}
 
-	//初始化边的距离和权重
+	//鍒濆鍖栬竟鐨勮窛绂诲拰鏉冮噸
 	for ( int i = 0; i < edge_num; i++){
 		int a, b, c, d;
 		cin >> a >> b >> c >> d;
@@ -71,11 +92,11 @@ int main() {
 		edge_weight[b][a] = d;
 	}
 
-	//初始化dist数组
+	//鍒濆鍖杁ist鏁扮粍
 	for (int i = 0; i < vertex_num; i++) {
 		dist[i] = graph[start][i];
 	}
-	//Dijkstra算法
+	//Dijkstra绠楁硶
 	for (int i = 0; i < vertex_num; i++) {
 		int u = -1, minn = INF;
 		for (int j = 0; j < vertex_num; j++) {
@@ -98,9 +119,9 @@ int main() {
 			else;
 		}
 	}
-	//深度优先遍历
+	//娣卞害浼樺厛閬嶅巻
 	dfs(start, end);
-	//输出结果
+	//杈撳嚭缁撴灉
 	print_path(path);
 	cout << dist[end] << " " << optimal_path_cost;
 

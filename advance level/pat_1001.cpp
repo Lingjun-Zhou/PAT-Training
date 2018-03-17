d2fceca96053fea3ecee31bb2d0b05ee16c66927
Zhou,Lingjun <zjjhyzzjc@163.com>
2018-03-17 23:04:11 +0800

    （20，0.28）字符串处理，int2string相互转换

--- advance level/pat_1001.cpp
+++ advance level/pat_1001.cpp
@@ -1,3 +1,17 @@
+1001.A+B Format(20)
+Calculate a + b and output the sum in standard format -- that is, the digits must be separated into groups of three by commas (unless there are less than four digits).
+Input
+Each input file contains one test case. Each case contains a pair of integers a and b where -1000000 <= a, b <= 1000000. The numbers are separated by a space.
+
+Output
+For each test case, you should output the sum of a and b in one line. The sum must be written in the standard format.
+Sample Input
+-1000000 9
+Sample Output
+-999,991
+//字符串处理，int2string相互转换
+
+
 #include<iostream>
 #include<string>
 #include<sstream>
@@ -17,7 +31,7 @@ int main() {
 	string output = int2str(sum);
 
 	int length = output.size();
-	
+
 	for (int i = length-3; i >= 0; i -= 3) {
 		if (i < 1)
 			continue;
